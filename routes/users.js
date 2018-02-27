ce600f40d60c112dbabebe2b857105331b6226f3
guoxiaobo <xiaobo.guo@kongapi.com>
2018-02-27 14:12:59 +0800

    mongo更新

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,9 @@ var userModel = require('../models/userModels.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.redirect('/users/list')
 });
+
 // 用户列表
 router.get('/list', function(req, res, next) {
   // var list = [
@@ -26,10 +27,23 @@ router.get('/list', function(req, res, next) {
   //   user: list
   // })
 })
+
+// 用户详情
+router.get('/detail/:id', function(req, res, next) {
+  var id = req.params.id;
+  userModel.findOne({_id: id}, function(err, data) {
+    if (err) {return console.log(err);}
+    res.render('UserDetail', {
+      user: data
+    })
+  })
+})
+
 // 添加用户
 router.get('/add', function(req, res, next) {
   res.render('UserAdd')
 })
+
 router.post('/add', function(req, res, next) {
   console.log(req, res)
   var newuser = new userModel({
@@ -39,6 +53,41 @@ router.post('/add', function(req, res, next) {
   newuser.save(function(err, data) {
     if (err) {return console.log(err)}
     res.redirect('/users/list')
+  })
+})
+
+// 更新用户信息
+router.get('/edit/:id', function(req, res, next) {
+  var id = req.params.id;
+  userModel.findOne({_id: id}, function(err, data) {
+    res.render('UserEdit', {
+      user: data
+    })
+  })
+})
+
+router.post('/update', function(req, res, next) {
+  var id = req.body.id
+  userModel.findById(id, function(err, data) {
+    if (err) {return console.log(err)}
+    data.username = req.body.username
+    data.email = req.body.email
+    data.save(function(err){
+      res.redirect('/users/list')
+    })
+  })
+})
+
+// 删除用户
+router.delete('/del', function(req, res) {
+  var id = req.query.id
+  userModel.remove({_id: id}, function(err, data){
+    if (err) {return console.log(err)}
+    res.json({
+      code: 200,
+      msg: '删除成功'
+    })
+    // res.redirect('/');
   })
 })
 
